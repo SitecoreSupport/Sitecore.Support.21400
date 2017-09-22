4b7e4c8b7db33449fe23f0dd96a414bc42deccbf
Alexander Lebedenko <ale@sitecore.net>
2017-09-22 10:01:52 +0300

    CommerceUserEventHandler causes unneeded requests to Commerce Server. Fix. Initial commit.

---
+++ src/Sitecore.Support.21400/CommerceUserEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Security;
+using Sitecore.Diagnostics;
+using Sitecore.Events;
+using Sitecore.Security.Accounts;
+using Sitecore.Security.Domains;
+
+namespace Sitecore.Support.Commerce.Connect.CommerceServer.Profiles.Events
+{
+    public class CommerceUserEventHandler : Sitecore.Commerce.Connect.CommerceServer.Profiles.Events.
+        CommerceUserEventHandler
+    {
+        public override void OnUserUpdated(object sender, EventArgs e)
+        {
+            Assert.ArgumentNotNull(sender, "sender");
+            Assert.ArgumentNotNull(e, "args");
+            MembershipUser user = Event.ExtractParameter(e, 0) as MembershipUser;
+            if (user != null && User.Exists(user.UserName))
+            {
+                string[] commerceDomains = MainUtil.RemoveEmptyStrings(Sitecore.Configuration.Settings.GetSetting("CommerceDomains").ToLowerInvariant().Split(new char[] {'|'}));
+                if (commerceDomains != null)
+                {
+                    foreach (string domain in commerceDomains)
+                    {
+                        if (Domain.GetAccountDomain(user.UserName).Name.ToLowerInvariant() == domain)
+                        {
+                            base.OnUserUpdated(sender, e);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.21400/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.21400")]
+[assembly: AssemblyProduct("Sitecore.Support.21400")]
+[assembly: ComVisible(false)]
